c87faf0b93b2bf46a22c477f3af9b243e5c1c00f
Tahmid Jawad An-Noor <tjawad98@gmail.com>
2023-12-09 01:25:43 +0600

    error

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,3 +1,3 @@
 module.exports = (error, req, res, next) => {
-    return res.status(400).send("Something went wrong!");
+    return res.status(400).send("Something went wrong. This is from central error!!");
 }
